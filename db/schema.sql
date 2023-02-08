ea72afc0b3b58592027905253ff19c2690bbe37a
TrianePeart <TrianePeart@gmail.com>
2023-02-08 12:47:35 -0500

    Updated Schema & Seed Feb.8.2023

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,34 @@
-DROP DATABASE IF EXISTS ;
-CREATE DATABASE 
+DROP DATABASE IF EXISTS write_tingz;
+CREATE DATABASE write_tingz;
 
-\c ; 
+\c write_tingz; 
 
-CREATE TABLE users (
-   id serial PRIMARY KEY, 
-   userName VARCHAR(60),
-   password TEXT, 
---   add admin after MVP is done. Likely Pool or PK
--- create table for TOS or add in. Google it. 
--- maybe should be named log in
---might put email or make it a seperate sign up. Circle back. 
-); 
+-- CREATE TABLE users (
+--    user_id serial PRIMARY KEY, 
+--    username VARCHAR(60) UNIQUE NOT NULL,
+--    --should allow for it to make only single sn
+--    email: VARCHAR UNIQUE NOT NULL,
+--    --this might be an issue take out uni if anything
+--    password TEXT NOT NULL 
+--    --Might be another Varchar 
+-- --   add admin after MVP is done. Likely Pool or PK
+-- -- create table for TOS or add in. Google it. 
+-- -- maybe should be named log in
+-- --might put email or make it a seperate sign up. Circle back. 
+-- ); 
+
+DROP TABLE IF EXISTS posts; 
+
+CREATE TABLE posts (
+   article_id SERIAL PRIMARY KEY, 
+   title VARCHAR(200) NOT NULL, 
+   author VARCHAR(60) NOT NULL,
+   content TEXT NOT NULL, 
+   post_type TEXT DEFAULT 'Select Post Type',
+   updated_at TIMESTAMP, 
+   created_at DATE,
+   is_liked BOOL ,
+   is_bookmarked BOOL
+);
+
+

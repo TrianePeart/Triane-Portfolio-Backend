8d8de893cc933827b342701a511fdeb79313bf58
TrianePeart <TrianePeart@gmail.com>
2023-02-17 08:04:34 -0500

    Trigger re-render

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -36,6 +36,7 @@ const cn = DATABASE_URL
       port: PG_PORT,
       database: PG_DATABASE,
       user: PG_USER,
+      password: process.env.PG_PASSWORD
     };
 
 const db = pgp(cn);
--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,6 @@ CREATE DATABASE write_tingz;
 
 \c write_tingz; 
 
--- CREATE TABLE users (
---    user_id serial PRIMARY KEY, 
---    username VARCHAR(60) UNIQUE NOT NULL,
---    --should allow for it to make only single sn
---    email: VARCHAR UNIQUE NOT NULL,
---    --this might be an issue take out uni if anything
---    password TEXT NOT NULL 
---    --Might be another Varchar 
--- --   add admin after MVP is done. Likely Pool or PK
--- -- create table for TOS or add in. Google it. 
--- -- maybe should be named log in
--- --might put email or make it a seperate sign up. Circle back. 
--- ); 
 
 DROP TABLE IF EXISTS posts; 
 
@@ -29,4 +16,5 @@ CREATE TABLE posts (
    is_bookmarked BOOL
 );
 
+-- DROP TABLE IF EX
 
